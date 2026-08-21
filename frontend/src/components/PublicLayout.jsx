391248fc00f3781975f148bdf081a3a37b3dffee
Emmanuel K <152315670+KDomos@users.noreply.github.com>
2026-08-22 01:14:53 +0300

    Update PublicLayout.jsx

--- frontend/src/components/PublicLayout.jsx
+++ frontend/src/components/PublicLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Masthead from "./Masthead";
 import Footer from "./Footer";
+import NotificationPrompt from "./NotificationPrompt";
 
 export default function PublicLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Masthead />
       <main className="flex-1 max-w-5xl w-full mx-auto px-5 py-10">
+        <NotificationPrompt />
         <Outlet />
       </main>
       <Footer />
